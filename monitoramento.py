e05d83028d58a07c97638238e213e7c4d56b27b1
Pedro Albertini <88116673+pedro-albertini@users.noreply.github.com>
2025-08-12 15:55:01 -0300

    Update monitoramento.py

--- monitoramento.py
+++ monitoramento.py
@@ -1,9 +1,10 @@
 import requests
 from datetime import datetime
+import subprocess
 
-URL = "http://172.22.64.106"
-TOKEN = "7573522611:AAFwDQCxhd2VD1uF6nop_xiYT6g6thrvBKg"
-CHAT_ID = "6801410508"
+URL = "SEU_IP"
+TOKEN = "SEU_TOKEN"
+CHAT_ID = "SEU_CHAT"
 MENSAGEM = "ALERTA!! Site fora do ar!"
 
 def mensagem_telegram(mensagem):
@@ -11,15 +12,24 @@ def mensagem_telegram(mensagem):
         data = {"chat_id": CHAT_ID, "text": mensagem}
         resposta = requests.post(url, data=data)
         return resposta.json()
-
+        
+def reiniciar_nginx():
+        subprocess.run(["sudo","systemctl","restart","nginx"],check=True)
+        print(f"{datetime.now()} - Servidor reiniciado com sucesso")
+        
 def monitoramento():
         try:
                 resposta = requests.get(URL, timeout=10)
                 if resposta.status_code == 200:
                         print(f"{datetime.now()} - Site está online")
+                else:
+                        mensagem_telegram(MENSAGEM)
+                        print(f"{datetime.now()} - Site está fora do ar")
+                        reiniciar_nginx()
         except requests.RequestException:
                 mensagem_telegram(MENSAGEM)
                 print(f"{datetime.now()} - Site está fora do ar")
-
+                reiniciar_nginx()
+                
 if __name__ == "__main__":
         monitoramento()
